6cd31751c408607516d0802255e53baeef4796d7
Edenbitran <eden.bitran@post.runi.ac.il>
2024-12-03 14:10:11 +0200

    all files

--- Primes.java
+++ Primes.java
@@ -4,14 +4,14 @@ public class Primes {
     public static void main(String[] args) {
         int lim = Integer.parseInt(args[0]);
         int j = 2;
-        boolean[] bool = new boolean[lim];
+        boolean[] bool = new boolean[lim + 1];
         for (int i = 0; i < bool.length; i++) { // if the index>1 it is true
             if (i > 1) {
                 bool[i] = true;
             }
             // System.out.printf("%8s", bool[i]);
         }
-        System.out.println();
+        //System.out.println();
         for (int i = 2; i <= bool.length; i++) {
             while (i % j == 0 && i > j) {
                 bool[i] = false;
@@ -33,7 +33,7 @@ public class Primes {
         }
 
         double presentage = (countP / lim) * 100;
-        System.out.println();
+        //System.out.println();
         System.out.println("Prime numbers up to " + lim + ":");
         System.out.println("There are " + (int) countP + " primes between 2 and " + lim + " (" + (int) presentage + "% are primes) ");
     }
